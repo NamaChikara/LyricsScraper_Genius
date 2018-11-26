78d79e59a494590ce42711824cbe1bf50a60fe2b
Zachary <zackbarry13@gmail.com>
2018-11-26 15:07:48 -0800

    Runs via command line and stores the location of a default directory for the user.

--- LyricsScraper_Genius.py
+++ LyricsScraper_Genius.py
@@ -1,21 +1,87 @@
+#! python3
+
+# LyricsScraper_Genius.py compiles a document of lyrics contained in a given
+#  album page on the popular lyrics site Genius.com.
+
+# To run this from the command line, type
+#       python C:\...\LyricsScraper_Genius.py album_url output_folder
+# where ... represents the path to LyricsScraper_Genius.py on your system,
+# album_url is a link to an album on Genius.com, and output_folder is where
+# you want the file saved to.
+
 import requests     # for downloading the webpage HTML sources
 import bs4          # for parsing the HTML
 import re           # regular expressions to extract artist/album name from url
 import sys          # exit in case of bad link
 import os           # for file management
 import webbrowser   # to open HTML document at the end
+import shelve       # to store the location of a default storage directory
 
 # --------------------------------------------------------------------------
 # load album_url and destination folder for output HTML file
 # --------------------------------------------------------------------------
-album_url = 'https://genius.com/albums/Kevin-morby/Singing-saw'
-output_folder = r'C:\Users\zackb_000\Documents\Programming Projects\LyricsScraper_Genius\SavedLyrics'
 
-# make sure folder is valid
+# load .dat file that is meant to save a default directory
+
+shelfFile = shelve.open(os.path.join(os.getcwd(), 'Destination.dat'))
+
+if 'DIRECTORY' not in list(shelfFile.keys()):
+    shelfFile['DIRECTORY'] = ''
+
+defaultDir = shelfFile['DIRECTORY']
+
+# attempt to load album_url and output_folder based on size of sys.argv
+
+album_url = ''
+output_folder = ''
+
+if len(sys.argv) == 3:
+    album_url = sys.argv[1]
+    output_folder = sys.argv[2]
+    # if the default directory is different, ask user if they would like to update it
+    if defaultDir != output_folder:
+        print('\n\nUpdate default save directory from\n\t%s\n--to--\n\t%s\n? (Y/N)' % (defaultDir, output_folder))
+        response = input()
+        if response == 'Y':
+            shelfFile['DIRECTORY'] = output_folder
+            print('Updated successfully.\n\n')
+        elif response == 'N':
+            print('Okay, it will remain the same.\n\n')
+    # if there is not a default directory, initialize one
+    elif not defaultDir:
+        shelfFile['DIRECTORY'] = output_folder
+elif len(sys.argv) == 2:
+    album_url = sys.argv[1]
+    # use the default directory if there is one
+    if defaultDir:
+        output_folder = defaultDir
+    else:
+        print('\n\nThere is no default save directory. Enter a path to a directory or type "EXIT".')
+        response = input()
+        if response == 'EXIT':
+            sys.exit()
+        else:
+            output_folder = response
+            print(response)
+            shelfFile['DIRECTORY'] = output_folder
+else:
+    print('\n Bad number of arguments.')
+    sys.exit()
+
+# make sure folder is valid, ask user to provide a valid folder if not
 
 if not os.path.isdir(output_folder):
-    print('Not a valid location to save the lyrics file.')
-    sys.exit()
+    print('\nNot a valid location to save the lyrics file.')
+    while True:
+        print('Please enter a valid folder, "EXIT" to quit.')
+        response = input()
+        if response == 'EXIT':
+            sys.exit()
+        else:
+            if os.path.isdir(response):
+                output_folder = response
+                break
+
 # --------------------------------------------------------------------------
 
 
